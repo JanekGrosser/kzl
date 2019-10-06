6089203d275875dc3bbec1a7043d8aae85d6f3c2
Jan Grosser <janek.grosser@gmail.com>
2019-10-06 10:59:06 +0200

    config tamplate file add comments

--- backend/config/config-template.js
+++ backend/config/config-template.js
@@ -1,22 +1,28 @@
-// Template for config.js
-// Use this template to indicate changes in cinfig file, but provide only non-sensitive values ex. password: "mypassword"
 "use-strict";
+// 
+// Template for config.js.
+// Use this template to indicate changes in config file, but provide only non-sensitive values ex.: password: "mypassword".
+// Uncomment the last line in real config.js file.
+// *
+// Change env value depending on enviroment ("dev", "test", "prod").
+const env = "dev";
 
-const env = 'dev';
-
+// Local development.
 const dev = {
 
 }
 
+// Test server.
 const test = {
 
 }
 
+// Production server.
 const prod = {
 
 }
 
-
 const config = { dev, test, prod };
-// Uncomment the line below in real config.js file
+
+// Uncomment the line below in real config.js file.
 // module.exports = config[env];
