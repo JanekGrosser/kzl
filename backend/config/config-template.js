edfed5f857d804561b07cc9b29cc0fef67a73ebc
Jan Grosser <janek.grosser@gmail.com>
2019-10-06 10:50:39 +0200

    add config template file

---
+++ backend/config/config-template.js
@@ -0,0 +1,22 @@
+// Template for config.js
+// Use this template to indicate changes in cinfig file, but provide only non-sensitive values ex. password: "mypassword"
+"use-strict";
+
+const env = 'dev';
+
+const dev = {
+
+}
+
+const test = {
+
+}
+
+const prod = {
+
+}
+
+
+const config = { dev, test, prod };
+// Uncomment the line below in real config.js file
+// module.exports = config[env];
