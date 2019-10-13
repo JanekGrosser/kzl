017ff86ba67070c659ade60a9b0159507326123a
Jan Grosser <janek.grosser@gmail.com>
2019-10-13 21:26:15 +0200

    add cors module

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const userRoutes = require("./routes/user.route");
 const authRoutes = require("./routes/auth.route");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
