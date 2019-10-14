97fc0b54b6df2fcb71d37e4e239731fc42fb23db
Jan Grosser <janek.grosser@gmail.com>
2019-10-14 19:14:00 +0200

    another test route

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -11,5 +11,7 @@ router.post("/resetpassword/:username_csr", authController.resetPassword);
 router.post("/newpassword/:user_id", authController.newPassword);
 //middleware test route
 router.get("/secret/headers", middleware.checkJwt,(req, res)=>{res.json(req.headers)});
+router.get("/headers",(req, res)=>{res.json(req.headers)});
 router.get("/secret/payload", middleware.checkJwt,(req, res)=>{res.json(req.payload)});
+
 module.exports = router;
