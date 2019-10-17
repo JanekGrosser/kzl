281efff5cdaa2f570aaea8990faf417deb9d7b14
Jan Grosser <janek.grosser@gmail.com>
2019-10-17 15:48:55 +0200

    add TODO comment

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -8,10 +8,12 @@ const middleware = require("../middleware/middleware");
 
 router.post("/login", authController.login);
 router.post("/resetpassword/:username_csr", authController.resetPassword);
-router.post("/newpassword/:user_id", authController.newPassword);
-//middleware test route
+router.post("/newpassword/:user_id", middleware.checkJwt, authController.newPassword);
+
+//middleware test routes 
+//TODO: remove at some point
 router.get("/secret/headers", middleware.checkJwt,(req, res)=>{res.json(req.headers)});
 router.get("/headers",(req, res)=>{res.json(req.headers)});
-router.get("/secret/payload", middleware.checkJwt,(req, res)=>{res.json(req.payload)});
+router.get("/secret/payload", middleware.checkJwt,(req, res)=>{res.json(res.locals.payload)});
 
 module.exports = router;
