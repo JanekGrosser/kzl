4390218dea9e6498708f1b676153a995055323ac
Jan Grosser <janek.grosser@gmail.com>
2019-10-17 15:47:21 +0200

    tiny refactors

--- backend/lib/user.js
+++ backend/lib/user.js
@@ -1,17 +1,18 @@
 "use-strict";
+//TODO add missing try catch blocks
+//TODO consider always using username_csr instead od user_id in requests
+//TODO add validator
 
-// const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const knex = require("../config/knex");
 const saltRounds = 10;
 
 exports.listAll = async (req, res) => {
-    //TODO remember to remove hash field from query
-    let users = await knex("user")
+    const users = await knex("user")
         .select("user_id", "username_csr","first_name", "last_name", "phone_num", "fp_name", "roles.role", "active")
         .innerJoin("fp", "user.fp_id", "fp.fp_id")
         .innerJoin("roles", "user.role_id", "roles.role_id");
-    res.status(200).json(users);
+    return res.status(200).json(users);
 };
 
 exports.newUser = async (req, res) => {
@@ -31,38 +32,42 @@ exports.newUser = async (req, res) => {
         && req.body.last_name 
         && req.body.phone_num 
         && req.body.fp_id
-        && req.body.role_id
-        )) {
-        res.status(400).send("Check request params");
+        && req.body.role_id)) {
+            return res.status(400).send("Check request params");
     } else {
-        let hash = await bcrypt.hash(password, saltRounds);
+        const hash = await bcrypt.hash(password, saltRounds);
         let active = 1;
         try {
             await knex("user").insert({ username_csr, first_name, last_name, phone_num, fp_id, active, hash, role_id });
         } catch (error) {
-            res.status(400).send(error.stack);
-            return;
+            return res.status(400).send(error.stack);
         }
-        res.status(201).send(`User ${username_csr} created`);
+        return res.status(201).send(`User ${username_csr} created`);
     };
 };
 
 exports.getUser = async (req, res) => {
-    let id = req.params.user_id;
+    const id = req.params.user_id;
     let user = await knex("user")
         .where({ user_id: id })
-        .first("user_id", "username_csr", "first_name", "last_name", "phone_num", "fp_name", "roles.role", "active")
+        .first("user_id", "username_csr", "first_name", "last_name", "phone_num", "fp_name", "role", "active")
         .innerJoin("fp", "user.fp_id", "fp.fp_id")
-        .innerJoin("roles", "user.role_id", "roles.role_id")
-    if (user) res.status(200).json(user);
-    else res.status(404).send(`User id ${id} not found`);
+        .innerJoin("roles", "user.role_id", "roles.role_id");
+    if (user) {
+        return res.status(200).json(user);
+    } else {
+        return res.status(404).send(`User id ${id} not found`);
+    };
 };
 
 exports.deleteUser = async (req, res) => {
     const id = req.params.user_id;
-    let user = await knex("user")
+    const user = await knex("user")
         .where({ user_id: id })
         .del();
-    if (user > 0) res.status(200).send(`User id: ${id} removed`);
-    else res.status(404).send(`User id ${id} not found`);
+    if (user > 0) {
+        return res.status(200).send(`User id: ${id} removed`);
+    } else {
+        return res.status(404).send(`User id ${id} not found`);
+    };
 };
