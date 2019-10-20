d4e4d4b015a728935e0fb784c34a225abb1228c6
Jan Grosser <janek.grosser@gmail.com>
2019-10-20 02:42:41 +0200

    database update

--- backend/models/db/Dump20191019-Structure.sql
+++ backend/models/db/Dump20191019-Structure.sql
@@ -29,7 +29,8 @@ CREATE TABLE `man_shifts` (
   `shift_id` tinyint(4) NOT NULL,
   `status_id` tinyint(4) NOT NULL,
   `user_last_change` datetime NOT NULL,
-  PRIMARY KEY (`man_shift_id`)
+  PRIMARY KEY (`man_shift_id`),
+  KEY `index2` (`user_id`,`date`,`shift_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -105,7 +106,6 @@ CREATE TABLE `users` (
   `first_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `phone_num` int(11) NOT NULL,
-  `subdivision_id` tinyint(4) NOT NULL,
   `active` tinyint(1) NOT NULL,
   `hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `role_id` tinyint(4) NOT NULL,
@@ -114,8 +114,30 @@ CREATE TABLE `users` (
   UNIQUE KEY `username_csr` (`username_csr`),
   KEY `fk_users_1_idx` (`role_id`),
   CONSTRAINT `fk_users_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=144 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=180 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary table structure for view `users_auth_view`
+--
+
+DROP TABLE IF EXISTS `users_auth_view`;
+/*!50001 DROP VIEW IF EXISTS `users_auth_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `users_auth_view` AS SELECT 
+ 1 AS `user_id`,
+ 1 AS `username_csr`,
+ 1 AS `first_name`,
+ 1 AS `last_name`,
+ 1 AS `phone_num`,
+ 1 AS `active`,
+ 1 AS `role_id`,
+ 1 AS `role`,
+ 1 AS `last_pass_reset`,
+ 1 AS `hash`,
+ 1 AS `user_subdivisions`*/;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `users_subdivisions`
@@ -125,15 +147,72 @@ DROP TABLE IF EXISTS `users_subdivisions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users_subdivisions` (
-  `user_id` smallint(5) unsigned NOT NULL,
-  `subdivision_id` tinyint(4) NOT NULL,
-  PRIMARY KEY (`user_id`,`subdivision_id`),
-  KEY `fk_users_has_subdivisions_subdivisions1_idx` (`subdivision_id`),
-  KEY `fk_users_has_subdivisions_users1_idx` (`user_id`),
-  CONSTRAINT `fk_users_has_subdivisions_subdivisions1` FOREIGN KEY (`subdivision_id`) REFERENCES `subdivisions` (`subdivision_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_has_subdivisions_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `users_user_id` smallint(5) unsigned NOT NULL,
+  `subdivisions_subdivision_id` tinyint(4) NOT NULL,
+  PRIMARY KEY (`users_user_id`,`subdivisions_subdivision_id`),
+  KEY `fk_users_has_subdivisions_subdivisions1_idx` (`subdivisions_subdivision_id`),
+  KEY `fk_users_has_subdivisions_users1_idx` (`users_user_id`),
+  CONSTRAINT `fk_users_has_subdivisions_subdivisions1` FOREIGN KEY (`subdivisions_subdivision_id`) REFERENCES `subdivisions` (`subdivision_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_subdivisions_users1` FOREIGN KEY (`users_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary table structure for view `users_view`
+--
+
+DROP TABLE IF EXISTS `users_view`;
+/*!50001 DROP VIEW IF EXISTS `users_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `users_view` AS SELECT 
+ 1 AS `user_id`,
+ 1 AS `username_csr`,
+ 1 AS `first_name`,
+ 1 AS `last_name`,
+ 1 AS `phone_num`,
+ 1 AS `active`,
+ 1 AS `role_id`,
+ 1 AS `role`,
+ 1 AS `last_pass_reset`,
+ 1 AS `user_subdivisions`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `users_auth_view`
+--
+
+/*!50001 DROP VIEW IF EXISTS `users_auth_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `users_auth_view` AS select `u`.`user_id` AS `user_id`,`u`.`username_csr` AS `username_csr`,`u`.`first_name` AS `first_name`,`u`.`last_name` AS `last_name`,`u`.`phone_num` AS `phone_num`,`u`.`active` AS `active`,`u`.`role_id` AS `role_id`,`r`.`role` AS `role`,`u`.`last_pass_reset` AS `last_pass_reset`,`u`.`hash` AS `hash`,group_concat(`s`.`subdivision_id` separator ', ') AS `user_subdivisions` from (((`users` `u` join `users_subdivisions` `us` on((`us`.`users_user_id` = `u`.`user_id`))) join `subdivisions` `s` on((`s`.`subdivision_id` = `us`.`subdivisions_subdivision_id`))) join `roles` `r` on((`u`.`role_id` = `r`.`role_id`))) group by `u`.`user_id` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `users_view`
+--
+
+/*!50001 DROP VIEW IF EXISTS `users_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `users_view` AS select `u`.`user_id` AS `user_id`,`u`.`username_csr` AS `username_csr`,`u`.`first_name` AS `first_name`,`u`.`last_name` AS `last_name`,`u`.`phone_num` AS `phone_num`,`u`.`active` AS `active`,`u`.`role_id` AS `role_id`,`r`.`role` AS `role`,`u`.`last_pass_reset` AS `last_pass_reset`,group_concat(`s`.`subdivision_id` separator ', ') AS `user_subdivisions` from (((`users` `u` join `users_subdivisions` `us` on((`us`.`users_user_id` = `u`.`user_id`))) join `subdivisions` `s` on((`s`.`subdivision_id` = `us`.`subdivisions_subdivision_id`))) join `roles` `r` on((`u`.`role_id` = `r`.`role_id`))) group by `u`.`user_id` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -144,4 +223,4 @@ CREATE TABLE `users_subdivisions` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-10-19 17:38:26
+-- Dump completed on 2019-10-20  2:42:13
