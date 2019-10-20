f76aa6dd1e87c61f3993b285777c8e9cf68292de
Jan Grosser <janek.grosser@gmail.com>
2019-10-20 02:59:32 +0200

    api for dictionary tables

--- backend/routes/data-routes.js
+++ backend/routes/data-routes.js
@@ -6,5 +6,10 @@ const dataController = require("../lib/data");
 const middleware = require("../middleware/middleware");
 
 router.get("/current-callendar/:user_id", middleware.checkJwt, middleware.hasRoleOrIdMatch(["adm", "koo", "koz", "pre"]), dataController.getCurrentShifts);
+router.get("/subdivisions", middleware.checkJwt, dataController.getSubdivisionsDictionary);
+router.get("/shifts", middleware.checkJwt, dataController.getShiftsDictionary);
+router.get("/roles", middleware.checkJwt, dataController.getRolesDictionary);
+router.get("/status", middleware.checkJwt, dataController.getStatusDictionary);
+
 
 module.exports = router;
