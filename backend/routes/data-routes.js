7b8021bbfdb3c78551fac5a75da53b5dac7a5ccd
Jan Grosser <janek.grosser@gmail.com>
2019-10-20 03:31:17 +0200

    small typo fix

--- backend/routes/data-routes.js
+++ backend/routes/data-routes.js
@@ -5,7 +5,7 @@ const dataController = require("../lib/data");
 
 const middleware = require("../middleware/middleware");
 
-router.get("/current-callendar/:user_id", middleware.checkJwt, middleware.hasRoleOrIdMatch(["adm", "koo", "koz", "pre"]), dataController.getCurrentShifts);
+router.get("/current-calendar/:user_id", middleware.checkJwt, middleware.hasRoleOrIdMatch(["adm", "koo", "koz", "pre"]), dataController.getCurrentShifts);
 router.get("/subdivisions", middleware.checkJwt, dataController.getSubdivisionsDictionary);
 router.get("/shifts", middleware.checkJwt, dataController.getShiftsDictionary);
 router.get("/roles", middleware.checkJwt, dataController.getRolesDictionary);
